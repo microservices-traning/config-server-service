246ea6d52ffd22ac5b08576588080fc51081be22
huyhn <huy.ha@formos.com>
2023-10-05 11:42:18 +0700

    Initial config-client properties

---
+++ src/test/java/com/huyhn/clone_config_server/CloneConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huyhn.clone_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
